7e0f2405512c854f74b64bf40dbb73ec7603519a
Monica Howard <jay.m.howard@gmail.com>
2018-12-03 21:14:07 -0600

    Connected to MongoDB using Mongoose

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://dbuser:dbuser1@ds139675.mlab.com:39675/basicexpressserver"
+};
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 // require dependencies
+// Add mongoose to dependencies
 const express = require("express");
+const mongoose = require("mongoose");
 
 // inititalize express app object
 const app = express();
+
+// Add db config file
+const db = require("./config/keys").mongoURI;
+
+// promise to connect to db
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 // when a request is made respond with message
 app.get("/", (req, res) => res.send("Hello, World"));
@@ -12,9 +23,3 @@ const port = process.env.PORT || 5000;
 
 // listen to port
 app.listen(port, () => console.log(`Express running on port ${port}`));
-
-// Add mongoose to dependencies
-
-// Add db config
-
-// promise to connect to db
